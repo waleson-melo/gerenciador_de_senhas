06c81ef25a18f250e572d1f36329ca7f672204d3
Waleson Melo <walesonmelo23@gmail.com>
2020-12-31 18:12:11 -0400

    Classe de conecção

---
+++ src/connect_database/connection.py
@@ -0,0 +1,37 @@
+import sqlite3
+
+
+class Connection:
+    def __init__(self):
+        self.createTables()
+
+    def connectDB(self):
+        self.conn = sqlite3.connect('data_base/dbsistema.db')
+        self.cursor = self.conn.cursor()
+
+    def desconectDB(self):
+        self.conn.commit()
+        self.cursor.close()
+
+    def createTables(self):
+        self.connectDB()
+        self.cursor.execute("""
+            CREATE TABLE "senhas" (
+                "id"	INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+                "nome"	TEXT NOT NULL,
+                "tipo"	TEXT NOT NULL,
+                "login"	TEXT NOT NULL,
+                "senha"	TEXT NOT NULL,
+                "observacoes"	TEXT
+            );
+        
+            CREATE TABLE IF NOT EXISTS "usuario" (
+                "id"	INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+                "cpf"	TEXT NOT NULL UNIQUE,
+                "nome"	TEXT NOT NULL,
+                "telefone"	TEXT,
+                "senha"	TEXT NOT NULL
+            );
+        """)
+
+        self.desconectDB()
