26620e2db980b07587f39dafe43c53a29efd6361
Waleson Melo <walesonmelo23@gmail.com>
2021-01-02 23:32:33 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import src.view.senhasView as sev
+# import src.view.senhasView as sev
+import src.controller.senhasController as sc
+
 
 if __name__ == '__main__':
-    sev = sev.SenhasView()
-    sev.start()
+    # sev = sev.SenhasView()
+    # sev.start()
+    sec = sc.SenhasController()
+    sec.start()
