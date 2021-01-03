5196d8199f28d65b8e4e94d776d36bb95177e616
Waleson Melo <walesonmelo23@gmail.com>
2021-01-03 13:00:02 -0400

    Create usuarioController.py

---
+++ src/controller/usuarioController.py
@@ -0,0 +1,26 @@
+import src.view.senhasView as sv
+import src.model.usuarioModel as um
+
+
+class UsuarioController:
+    def __init__(self):
+        self.usuario_model = um.UsuarioModel()
+
+    def start(self):
+        sev = sv.SenhasView(self)
+        sev.start()
+
+    def searchAllUsuarios(self):
+        return self.usuario_model.selectAll()
+
+    def searchUsuario(self, cpf):
+        return self.usuario_model.select(cpf)
+
+    def saveUsuario(self, cpf, nome, telefone, senha):
+        return self.usuario_model.save(cpf, nome, telefone, senha)
+
+    def updateUsuario(self, codigo, cpf, nome, telefone, senha):
+        return self.usuario_model.update(codigo, cpf, nome, telefone, senha)
+
+    def deleteUsuario(self, codigo):
+        return self.usuario_model.delete(codigo)
