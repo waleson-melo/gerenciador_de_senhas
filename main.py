3ec69a5442863c52d6fbeae6b0a3362f8867e4af
Waleson Melo <walesonmelo23@gmail.com>
2021-01-04 12:46:25 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,5 @@ import src.controller.mainWindowController as mwc
 
 
 if __name__ == '__main__':
-    # sec = sc.SenhasController()
-    # sec.start()
     mec = mwc.MainWindowController()
     mec.run()
